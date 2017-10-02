61a600f3e43fd865744e27e1e1f314c5f1f716e7
kidvan777 <ivankidyaev.ik@gmail.com>
2017-10-02 20:47:00 +0300

    up

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,14 +3,19 @@ import VideolistItem from './video_list_item';
 
 const Videolist = (props)=>{
   const videoItems = props.videos.map((video)=>{
-    return < VideolistItem  key={video.etag} video={video}/>
-  })
+    return (
+      <VideolistItem
+        onVideoSelect={props.onVideoSelect}
+        key={video.etag}
+        video={video}/>
+    )
+  });
 
   return(
     <ul className="col-md-4 list-group">
       {videoItems}
     </ul>
   )
-}
+};
 
 export default Videolist;
